c5dff1323e7deb5c0268624fefd1bf4b59c32e59
monalisa <monalisaganguly620@gmail.com>
2022-10-16 09:51:06 +0530

    first changes of this git learning

---
+++ Images/uploadImageTodrive.go
@@ -0,0 +1,9 @@
+package images
+
+import "fmt"
+
+//  Image nebe automatically imahe gogle drive e upload kore debe
+func main() {
+	fmt.Println("Uploading")
+	fmt.Println("To google drive...")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I am Monalisa")
+	fmt.Println("I am Rupam")
+
+}
